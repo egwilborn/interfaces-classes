64372125405da270df57f54aff46d58a138217c7
egwilborn <egwilborn@gmail.com>
2023-09-20 10:18:14 -0500

    adds fields

--- classes.ts
+++ classes.ts
@@ -1,19 +1,27 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-  honk(): void {
+  constructor(public color: string) {}
+  protected honk(): void {
     console.log("beep");
   }
 }
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
 // class car will inherit all the same functions of Vehicle
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   // you can override the methods in the child class
-  drive(): void {
+  //adding private keyword only allows you to call the drive method within this class
+  private drive(): void {
     console.log("vroom");
+  }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
-const car = new Car();
-car.drive();
-car.honk();
+const car = new Car(4, "red");
+car.startDrivingProcess();
